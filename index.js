8f564d21dd49be63886bfe220704d9cb8fd3554a
neparkes-81 <nparkes8165@gmail.com>
2022-03-02 12:27:34 -0500

    Added toggle on displayed forms and link decoding

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-document.querySelector('form'). addEventListener('submit', event => {
+const {hash} = window.location;
+const message = atob(hash.replace('#', ''));
+
+if(message){
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
 
     const messageInput = document.querySelector('#message-input');
     const encrypted = btoa(messageInput.value);
