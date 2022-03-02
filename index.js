3ad889edf495fb79e6eb309567a121f3bf5f4cfa
neparkes-81 <nparkes8165@gmail.com>
2022-03-02 11:58:48 -0500

    initial commit with general set up

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('form'). addEventListener('submit', event => {
+    event.preventDefault();
+
+    const messageInput = document.querySelector('#message-input');
+    const encrypted = btoa(messageInput.value);
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
+})
